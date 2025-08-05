525a353904afd38d8927f517c450750d37c1258d
Fabio Pinciroli <fabio@pinciroli.xyz>
2025-08-05 17:55:04 +0200

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,79 @@
+
+#include <FastLED.h>
+
+#define NUM_LEDS    30  //Strip lenght
+#define BRIGHTNESS  255 //Grand master
+
+CRGB leds[NUM_LEDS];  //Framebuffer
+
+enum stateEnum {  //Master state
+  avail,
+  toAvail,
+  busy,
+  toBusy
+};
+stateEnum state = avail;
+
+int animFrame;  //Animation frame index
+
+void setup() {
+  //Setup serial
+  Serial.begin(115200);
+
+  //Setup LED strip
+  FastLED.addLeds<WS2811, 5, GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(BRIGHTNESS);
+
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Green;
+  }
+
+  FastLED.show();
+
+}
+
+void loop()
+{
+  //Handle serial input
+  switch (Serial.read())
+  {
+    case 48: //(ASCII 0) Available
+      state = toAvail;
+      animFrame = 0;
+      break;
+
+    case 49: //(ASCII 1) Busy
+      state = toBusy;
+      animFrame = 0;
+      break;
+    
+    default:  //If invalid data byte, do nothing
+      break;
+  }
+  
+  
+  //Render animantion to framebuffer
+  switch (state) {
+    case toAvail: //Transitioning to green
+        leds[animFrame] = CRGB::Green;  //Set next pixel to green
+        animFrame++;
+
+        if(animFrame == NUM_LEDS)  //If at end of strip, go to steady state
+          state = avail;
+      break;
+
+    case toBusy: //Transitioning to red
+      leds[animFrame] = CRGB::Red;  //Set next pixel to green
+      animFrame++;
+
+      if(animFrame == NUM_LEDS)  //If at end of strip, go to steady state
+        state = avail;
+    break;
+    
+    default:  //For steady state, do nothing
+      break;
+  }
+
+  FastLED.show(); //Output frame
+  delay(50); //Sleep for 250ms
+}
